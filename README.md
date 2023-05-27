2c9df8264a0f8b61204421e3f4ecf4167d197f71
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2023-05-27 11:07:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lifecyclesObserver_ioslec
---
+++ lifecycleObservers/lifecycleObservers.h
@@ -0,0 +1,18 @@
+//
+//  lifecycleObservers.h
+//  lifecycleObservers
+//
+//  Created by Yevhenii Veretennikov on 27.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for lifecycleObservers.
+FOUNDATION_EXPORT double lifecycleObserversVersionNumber;
+
+//! Project version string for lifecycleObservers.
+FOUNDATION_EXPORT const unsigned char lifecycleObserversVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lifecycleObservers/PublicHeader.h>
+
+
